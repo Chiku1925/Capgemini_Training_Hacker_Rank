3fea02df68576542d1cd540685de9787a12a590b
Kaustuk Saraf <kaustuksaraf135@gmail.com>
2025-05-20 10:00:00 +0530

    Day1-Basics

---
+++ Question1.java
@@ -0,0 +1,10 @@
+package org.example.Day1;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
